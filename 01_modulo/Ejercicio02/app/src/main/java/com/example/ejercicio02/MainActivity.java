be59afb92c0390878d5c03e133c2e1e3e3dd175b
Luis Cobian <coby10@gmail.com>
2019-04-11 15:04:42 -0500

    Ejercicio
    Hasta b9oton

--- 01_modulo/Ejercicio02/app/src/main/java/com/example/ejercicio02/MainActivity.java
+++ 01_modulo/Ejercicio02/app/src/main/java/com/example/ejercicio02/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.ejercicio02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtNumero1)
+    EditText txtNyumero1;
+    @BindView(R.id.txtNumero2)
+    EditText txtNyumero2;
+
+    @BindView(R.id.rdSuma)
+    RadioButton rdSuma;
+    @BindView(R.id.rdResta)
+    RadioButton rdResta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.btnCalculo)
+    public void hacerCalculo(){
+        //rdSuma.isChecked()
+        Toast.makeText(this, "Presione",
+                 Toast.LENGTH_LONG).show();
     }
 }
